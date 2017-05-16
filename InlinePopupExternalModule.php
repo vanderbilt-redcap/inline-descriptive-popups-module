7a161535c54f6a9b52f0deb924092665048b6040
Mark McEver <mark@fastmail.net>
2017-05-16 10:28:36 -0500

    Switch to tippy popups, and change the behavior to search and replace all matching text with links.

--- InlinePopupExternalModule.php
+++ InlinePopupExternalModule.php
@@ -9,67 +9,91 @@
 namespace ExternalModules;
 require_once dirname(__FILE__) . '/../../external_modules/classes/ExternalModules.php';
 class InlinePopupExternalModule extends AbstractExternalModule {
-	static $jsIncluded = false;
 
 	function hook_data_entry_form($project_id) {
-		$this->includeJavascript();
-
-		$projectSettings = ExternalModules::getProjectSettingsAsArray($this->PREFIX,$project_id);
-
-//		var_dump($projectSettings);
-
-		echo '<div style="display:none" id="popupDisplayModal"></div>';
-
-		foreach($projectSettings["on-data-entry-forms"]['value'] as $instanceKey => $displayOnSurvey) {
-			if($displayOnSurvey) {
-				$this->generateFormPopup($project_id,$instanceKey);
-			}
-		}
+		$this->includeSharedCode($project_id, 'on-data-entry-forms');
 	}
 
 	function hook_survey_page($project_id) {
-		$this->includeJavascript();
+		$this->includeSharedCode($project_id, 'on-surveys');
+	}
 
-		$projectSettings = ExternalModules::getProjectSettingsAsArray($this->PREFIX,$project_id);
+	function includeSharedCode($project_id, $enabledSettingName) {
+		?>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/0.3.0/tippy.css" integrity="sha256-s2OFSYvBLhc6NZMWLBpEmjCS9bI27OoN1ckY1z7Z/3w=" crossorigin="anonymous" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/0.3.0/tippy.js" integrity="sha256-eW1TBvNkruqAr5vlt8AwpDDHmoCSq8yfxIpveZ+LO9o=" crossorigin="anonymous"></script>
 
-//		var_dump($projectSettings);
-
-		echo '<div style="display:none" id="popupDisplayModal"></div>';
-
-		foreach($projectSettings['on-surveys']['value'] as $instanceKey => $displayOnForm) {
-			if($displayOnForm) {
-				$this->generateFormPopup($project_id,$instanceKey);
+		<style>
+			.tippy-popper{
+				outline: 0;
 			}
-		}
-	}
 
-	function generateFormPopup($project_id,$instanceKey) {
+			.tippy-popper img{
+				max-width: 100%;
+			}
+
+			.tippy-tooltip.light-theme{
+				color: black;
+				font-size: 14px;
+				max-width: 500px;
+				text-align: left;
+			}
+
+			a[tooltip]{
+				cursor: pointer;
+			}
+		</style>
+		<?php
+
 		$projectSettings = ExternalModules::getProjectSettingsAsArray($this->PREFIX,$project_id);
 
-		$field = $projectSettings['name']['value'][$instanceKey];
-		$textLocation = $projectSettings['location']['value'][$instanceKey];
-		$text = $projectSettings['text']['value'][$instanceKey];
+		$number = 1;
+		foreach($projectSettings['link-text']['value'] as $instanceKey => $linkText) {
+			if($projectSettings[$enabledSettingName]['value'][$instanceKey] != 1){
+				continue;
+			}
 
-		if($field && $textLocation && $text) {
-			echo "<script type='text/javascript'>
-				ExternalModules.popupTexts.push('".htmlspecialchars($text,ENT_QUOTES)."');
-				ExternalModules.popupLocations.push('".htmlspecialchars($textLocation,ENT_QUOTES)."');
-				ExternalModules.popupFields.push('".htmlspecialchars($field,ENT_QUOTES)."');
-			</script>";
+			$text = $projectSettings['text']['value'][$instanceKey];
+
+			if(!empty($linkText) && !empty($text)) {
+				?>
+				<div id="popup-content-<?=$number?>" style="display: none"><?=$text?></div>
+				<script>
+					$(function(){
+						$('.labelrc').each(function(){
+							var label = $(this)
+
+							if(label.closest('tr.surveysubmit').length != 0){
+								// Don't replace the survey buttons (they might have associated event handlers).
+								return
+							}
+
+							label.html(label.html().replace(/<?=preg_quote($linkText)?>/g, "<a tooltip='<?=$number?>'>" + <?=json_encode($linkText)?> + "</a>"))
+						})
+					})
+				</script>
+				<?php
+			}
+
+			$number++;
 		}
-	}
 
-	function includeJavascript() {
-		if(!self::$jsIncluded) {
-			ExternalModules::addResource('js/globals.js');
-
-//			echo "Start len: ".strlen(__DIR__)." small size: ".strlen(dirname(dirname(__DIR__)))."<br />";
-
-			$moduleFolder = substr(__DIR__,strlen(dirname(dirname(__DIR__))));
-			$moduleUrl = APP_PATH_WEBROOT_FULL."/".$moduleFolder;
-			echo "<script src='".$moduleUrl."/js/addPopup.js'></script>";
-
-			self::$jsIncluded = true;
-		}
+		?>
+		<script>
+			$(function(){
+				// The Tippy calls cannot be inside the same loop as the replace calls, because replacing elements undoes any previous Tippy calls.
+				$('a[tooltip]').each(function() {
+					new Tippy(this, {
+						html: 'popup-content-' + $(this).attr('tooltip'),
+						trigger: 'mouseenter',
+	//					trigger: 'click',
+						theme: 'light',
+						arrow: true,
+						interactive: true
+					})
+				})
+			})
+		</script>
+		<?php
 	}
 }
